cc4d7e76456aec8ff39765d9a943d8bafad4d931
Miguel Angel Rojo <freemanz1486@gmail.com>
2025-05-26 11:41:30 +0100

    feat: `mempool` endpoints  (#43)
    * first commit
    
    * added galactica devnet
    
    * small refactor
    
    * devkit updated to last version
    
    * small modification
    
    * more changes
    
    * more changes
    
    * using dynamic fee calculation
    
    * usign dynamic fee transactions (to be fully tested using parse and other endpoints)
    
    * fixed error
    
    * removed redundant undefined
    
    * fixed combine
    
    * using master now
    
    * added middlewares, default mode is online
    
    * small refactor
    
    * small refactor
    
    * added e2e tests
    
    * tests modified to match values 1
    
    * tests modified to match values 2
    
    * tests modified to match values 3
    
    * linter
    
    * refactor
    
    * tests modified to match values 4
    
    * tests modified to match values 5
    
    * some fixes
    
    * gh actions scaffolding, yet to be tested
    
    * gh actions scaffolding, yet to be tested
    
    * gh actions scaffolding, yet to be tested
    
    * gh actions scaffolding, yet to be tested
    
    * gh actions scaffolding, yet to be tested
    
    * gh actions scaffolding, yet to be tested
    
    * sonar config
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * test gh
    
    * added case for basefee 0 and dynamic
    
    * added case for basefee 0 and dynamic
    
    * using master back
    
    * refactor
    
    * test: waiting for galactica
    
    * enabled txpool endpoints
    
    * using by default galactica release version in dockerfile
    
    * first iteration
    
    * first iteration
    
    * first iteration
    
    * added first endpoint
    
    * more changes
    
    * origin is optional
    
    * origin is optional
    
    * origin is optional
    
    * improvement
    
    * using existing transaction type
    
    * convertclauses method moved to a common place
    
    * refator
    
    * refactor
    
    * first version of the tests
    
    * added check
    
    * added check
    
    * sonar smells
    
    * code review comments

--- src/controllers/mempool.ts
+++ src/controllers/mempool.ts
@@ -1,9 +1,105 @@
+import axios from 'axios';
+import Joi from 'joi';
 import Router from "koa-router";
+import { getError } from '../common/errors';
+import { TransactionConverter } from '../common/transConverter';
+import { ConvertJSONResponseMiddleware } from '../middlewares/convertJSONResponseMiddleware';
+import { RequestInfoVerifyMiddleware } from '../middlewares/requestInfoVerifyMiddleware';
+import ConnexPro from '../utils/connexPro';
 
-export class Mempool extends Router {  
-    constructor(_env:any){
+
+export class Mempool extends Router {
+    private readonly env:any;
+    private readonly connex:ConnexPro;
+    private readonly verifyMiddleware:RequestInfoVerifyMiddleware;
+    private readonly transConverter:TransactionConverter;
+
+    constructor(env:any){
         super();
-        this.post('/mempool', async (_ctx,_next) => {});
-        this.post('/mempool/transaction',async (_ctx,_next) => {});
+        this.env = env;
+        this.connex = this.env.connex;
+        this.verifyMiddleware = new RequestInfoVerifyMiddleware(this.env);
+        this.transConverter = new TransactionConverter(this.env);
+        this.post('/mempool',
+            async (ctx,next) => { await this.verifyMiddleware.checkNetwork(ctx,next);},
+            async (ctx,next) => { await this.verifyMiddleware.checkRunMode(ctx,next);},
+            async (ctx,next) => { await this.verifyMiddleware.checkModeNetwork(ctx,next);},
+            async (ctx,next) => { await this.getTxPoolTransactions(ctx,next);}
+        );
+        this.post('/mempool/transaction', 
+            async (ctx,next) => { await this.verifyMiddleware.checkNetwork(ctx,next);},
+            async (ctx,next) => { await this.verifyMiddleware.checkRunMode(ctx,next);},
+            async (ctx,next) => { await this.verifyMiddleware.checkModeNetwork(ctx,next);},
+            async (ctx,next) => { await this.getTxPoolTransaction(ctx,next);}
+        );
+    }
+
+    private async getTransactions(origin?:string, expanded: boolean = false):Promise<Connex.Thor.Transaction[] | string[]> {
+        const baseUrl = this.connex.baseUrl + '/node/txpool';
+        const expandedParam = expanded ? '?expanded=true' : '';
+        const originParam = expanded ? '&' : '?';
+        const originQuery = origin ? `${originParam}origin=${origin}` : '';
+        const url = `${baseUrl}${expandedParam}${originQuery}`;
+        
+        const response = await axios.get(url);
+        const transactions = expanded ? response.data as Connex.Thor.Transaction[] : response.data as string[];
+
+        return transactions;
+    }
+
+    private checkMetadata(ctx:Router.IRouterContext):boolean{
+        const schema = Joi.object({
+            metadata: Joi.object({
+                origin: Joi.string().lowercase().length(42).regex(/^(-0x|0x)?[0-9a-f]*$/)
+            }).optional()
+        }).unknown(true);
+        const verify = schema.validate(ctx.request.body);
+        if(verify.error == undefined){
+            return true;
+        } else {
+            ConvertJSONResponseMiddleware.KnowErrorJSONResponse(ctx,getError(16,undefined,{
+                error:verify.error
+            }));
+            return false;
+        }
+    }
+
+    private async getTxPoolTransactions(ctx:Router.IRouterContext,next: () => Promise<any>){
+        if(this.checkMetadata(ctx)){
+            const txPool = await this.getTransactions(ctx.request.body.metadata?.origin) as string[];
+            ConvertJSONResponseMiddleware.BodyDataToJSONResponse(ctx,{
+                transaction_identifiers:txPool.map((tx) => {
+                    return {
+                        hash:tx
+                    }
+                })
+            });
+        }
+        await next();
+    }
+
+    private async getTxPoolTransaction(ctx:Router.IRouterContext,next: () => Promise<any>){
+        if(this.verifyMiddleware.checkTransactionIdentifier(ctx)){
+            const txPool = await this.getTransactions(undefined,true) as Connex.Thor.Transaction[];
+            const tx = txPool.find((tx) => {
+                return tx.id == ctx.request.body.transaction_identifier.hash;
+            });
+            if(tx == undefined){
+                ConvertJSONResponseMiddleware.KnowErrorJSONResponse(ctx,getError(25,undefined,{
+                    error: 'Transaction not found in mempool'
+                }));
+            } else {
+                const operations = await this.transConverter.convertClausesToOperations(tx);
+                ConvertJSONResponseMiddleware.BodyDataToJSONResponse(ctx,{
+                    transaction: {
+                        transaction_identifier: {
+                            hash: tx.id
+                        },
+                        operations
+                    }
+                });
+            }
+        }
+        await next();
     }
 }
---
+++ tests/e2e/controllers/mempool.test.ts
@@ -0,0 +1,59 @@
+import { TransactionIdentifier } from '../../../src/common/types/identifiers';
+import { client, galacticaDevnetNetworkIdentifier } from '../setup';
+
+describe('Mempool Controller', () => {
+    describe('POST /mempool', () => {
+        it('should return a list of pending transactions', async () => {
+            const response = await client.post('/mempool', {
+                network_identifier: galacticaDevnetNetworkIdentifier
+            });
+
+            expect(response).toHaveProperty('transaction_identifiers');
+            expect(response.transaction_identifiers).toBeInstanceOf(Array);
+            response.transaction_identifiers.forEach((tx: TransactionIdentifier) => {
+                expect(tx.hash).toMatch(/^0x[0-9a-f]{64}$/);
+            });
+        });
+    });
+
+    describe('POST /mempool/transaction', () => {
+        it('should return transaction details for a specific transaction', async () => {
+            const mempoolResponse = await client.post('/mempool', {
+                network_identifier: galacticaDevnetNetworkIdentifier
+            });
+
+            expect(mempoolResponse.transaction_identifiers).toBeInstanceOf(Array);
+            expect(mempoolResponse.transaction_identifiers.length).toBeGreaterThan(0);
+            
+            const txHash = mempoolResponse.transaction_identifiers[0].hash;
+            const response = await client.post('/mempool/transaction', {
+                network_identifier: galacticaDevnetNetworkIdentifier,
+                transaction_identifier: {
+                    hash: txHash
+                }
+            });
+
+            expect(response).toHaveProperty('transaction');
+            expect(response.transaction).toHaveProperty('transaction_identifier');
+            expect(response.transaction.transaction_identifier.hash).toBe(txHash);
+            expect(response.transaction).toHaveProperty('operations');
+            expect(response.transaction.operations).toBeInstanceOf(Array);
+        });
+
+        it('should return error for non-existent transaction', async () => {
+            let error: Error | undefined;
+            try {
+                await client.post('/mempool/transaction', {
+                    network_identifier: galacticaDevnetNetworkIdentifier,
+                    transaction_identifier: {
+                        hash: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef'
+                    }
+                });
+            } catch (e) {
+                error = e as Error;
+            }
+            expect(error).toBeDefined();
+            expect(error?.message).toContain('API Error: 500');
+        });
+    });
+}); 
